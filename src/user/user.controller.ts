bb9ff4fec7824e98cee1ac036b83b1fe2c6d353c
nsanzimfura-eric <erickykress1@gmail.com>
2023-12-12 22:42:57 +0200

    Add userModel and create its service and controllers to save the registerd user in the DB

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    try {
+      const user = await this.userService.createUser(createUserDto);
+      return user;
+    } catch (error) {
+      // Handle errors, e.g., duplicate email
+      throw new HttpException('Failed to create user', HttpStatus.BAD_REQUEST);
+    }
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
+  }
+}
