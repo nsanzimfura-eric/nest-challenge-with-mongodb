39c9c0b1464399fa21423962e47220cf76451f5e
nsanzimfura-eric <erickykress1@gmail.com>
2023-12-13 13:00:50 +0200

    Generate weather module, controllers, and services to work on fetch api

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { WeatherModule } from './weather/weather.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URI),
     UserModule,
+    WeatherModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  welcomeMessage(): string {
+    return 'Welcome to the Weather api, to to "/weather" and get all weather conditions!';
   }
 }
